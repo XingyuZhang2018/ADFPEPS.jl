a4f4f082a62cf103df832a679363271af6b0df6a
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2023-12-10 12:09:00 +0800

    update_row! and update_column!

--- src/SU.jl
+++ src/SU.jl
@@ -1,3 +1,6 @@
+using TeneT: qrpos, svd!, invDiagU1Matrix
+using LinearAlgebra
+
 @with_kw struct SU <: Algorithm
     dτ::Float64 = 0.4
     tratio::Float64 = 0.7
@@ -9,28 +12,229 @@
     count_lower::Int64 = 100
 end
 
-function initλΓ(SD, D, d)
-    λ = [Iinitial(SD, D; dir = [-1,1]) for _ in 1:4]
-    Γ = [randinitial(SD, d, D, D, D, D; dir = [1, -1, -1, 1, 1]) for _ in 1:2]
+function initλΓ(ST, D, d)
+    λ = [Iinitial(ST, D; dir = [-1,1]) for _ in 1:4]
+    Γ = [randinitial(ST, d, D, D, D, D; dir = [1, -1, -1, 1, 1]) for _ in 1:2]
     normalize!(Γ)
     return λ, Γ
 end 
 
-function evoGate(SD, model, dτ)
-    h = SD.atype{SD.dtype}.(hamiltonian(model))
- 
-    U_local = exp(-0.5*dτ*h[2]) 
-    function gen_U_2sites( h, dτ )
-        d = size(h)[1]
-        temp = reshape(h, d^2,d^2)
-        temp = exp(-1.0 * dτ * temp) 
-        temp = reshape(temp, d,d,d,d)
-        return temp
-    end
-    U_2sites_1 = gen_U_2sites(h[1], 0.5*dτ) 
-    
-    U_2sites_1 = asSymmetryArray(U_2sites_1, SD.symmetry, SD.stype; dir = [-1,-1,1,1]) 
-    U_local = asSymmetryArray(U_local, SD.symmetry, SD.stype; dir = [-1,1])   
-    
-    return U_local, U_2sites_1
+order(a) = a<5 ? a : (a-4)
+
+function qndims(ΓA, ind)     
+    indqn_D = Int64[]
+    indims_D = Int64[]
+    qn = ΓA.qn 
+    dims = ΓA.dims 
+    for i in 1:length(ΓA.qn)
+        if qn[i][ind] in indqn_D
+            continue
+        end 
+        push!(indqn_D, qn[i][ind]) 
+        push!(indims_D, dims[i][ind]) 
+    end   
+    return indqn_D, indims_D
 end
+
+function evoGate(ST, model, dτ)
+    h = ST.atype{ST.dtype}.(hamiltonian(model))
+    d = size(h[1], 1)
+    
+    U_2sites = reshape(exp(-0.5 * dτ * reshape(h[1], d^2,d^2)), d,d,d,d)
+    U_2sites = asSymmetryArray(U_2sites, ST.symmetry, ST.stype; dir = [-1,-1,1,1]) 
+
+    U_local = exp(-0.5 * dτ * h[2]) 
+    U_local = asSymmetryArray(U_local, ST.symmetry, ST.stype; dir = [-1,1])   
+    
+    return U_2sites, U_local
+end
+
+# fig form https://journals.aps.org/prb/abstract/10.1103/PhysRevB.81.165104
+function update_row!(ST, Γ, λ, Udτ, D_truc; whichbond)
+    sitetype, atype, dtype = ST.stype, ST.atype, ST.dtype
+    # up: left=1, lm=1
+    # down: left=0, lm=3
+    if whichbond == "right"
+        left = 1
+        right = 2
+        lm = 1
+    elseif whichbond == "left"
+        left = 2
+        right = 1
+        lm = 3
+    else
+        println("Wrong Position in update row!")
+    end
+    
+    ΓA = Γ[left]
+    ΓB = Γ[right]
+
+    D = ΓA.size[4]
+    d = Udτ.size[1]
+
+    indqn_D, indims_D = qndims(ΓA, 4) 
+    indqn_d, indims_d = getqrange(sitetype, d), getblockdims(sitetype, d)
+
+    swap_dD = U1swapgate(atype, dtype, d, D; 
+                        indqn = [indqn_d..., indqn_D, indqn_d..., indqn_D], 
+                        indims = [indims_d..., indims_D, indims_d..., indims_D],
+                        ifZ2=sitetype.ifZ2
+                        ) 
+    # step 1: Fig.29(a)
+    ΓAA = ein"pludr,pdsy -> sluyr"(ΓA, swap_dD) 
+
+    # step 2, Fig.34(a): obtain left part Θ  clockwise: dlurs 
+    Γ_0 = ein"((sludr,dx),yl),zu -> xyzrs"(ΓAA, λ[order(lm+1)], λ[order(lm+2)], λ[order(lm+3)])
+ 
+    # step 2': Fig.29(c) SVD/QR with no truncation 
+    Γ_0 = reshape(Γ_0, D^3, D*d) 
+    Q, R = qrpos(Γ_0)
+    
+    WA = reshape(Q, D, D, D, D*d) 
+    MA = reshape(R, D*d, D, d)
+
+    # step 3, Fig.34(a): right part Γ clock-wise   slurd
+    Γ_1 = ein"((pludr,xu),ry),dz -> plxyz"(ΓB, λ[order(lm+1)], λ[order(lm+2)], λ[order(lm+3)])
+
+    # step 3': Fig.29(d) SVD/RQ with no truncation      
+    Q, R = qrpos(reshape(Γ_1, d*D, D^3)')
+    UB = reshape(Q', D*d, D, D, D)
+    NB = reshape(R', d, D, D*d)
+
+    # step 4, Fig.34(a) or Towards Fig B.1(II)
+    Θ0 = ein"(lrs,rx),pxy -> lspy"(MA, λ[lm], NB) # anticlock-wise,  
+  
+    Θ = ein"lspr,psqt -> ltqr"(Θ0, Udτ) # anticlock-wise,
+     
+    shape = Θ.size
+    temp = reshape(Θ, shape[2]*shape[1], shape[3]*shape[4]) #  
+    U, S, V = svd!(temp; trunc=D_truc, middledir=1)  #  
+
+    MA_new = reshape(U, shape[1], shape[2], D_truc)
+    NB_new = reshape(V', D_truc, shape[3], shape[4])
+
+    S = Diagonal(S) 
+
+    # step 5, Fig.(35)
+    inv_λ2 = invDiagU1Matrix(λ[order(lm+1)])
+    inv_λ3 = invDiagU1Matrix(λ[order(lm+2)])
+    inv_λ4 = invDiagU1Matrix(λ[order(lm+3)])
+ 
+    #  WA:(d,l,u,r)  # MA_new:(lpr)
+    ΓAA_new = ein"(((dlur,dx),yl),zu),rpw -> pyzxw"(WA, inv_λ2, inv_λ3, inv_λ4, MA_new)  
+    # NB_new:(lpr)  # UB:(l,u,r,d )
+    ΓB_new = ein"(((mpl,lurd),xu),ry),dz -> pmxzy"(NB_new, UB, inv_λ2, inv_λ3, inv_λ4)
+ 
+  
+    # step 6, Fig.(32b)
+    indqn_D, indims_D = qndims(ΓAA_new, 4)  
+    swap_dD = U1swapgate(atype, dtype, d, D; 
+                        indqn = [indqn_d..., indqn_D, indqn_d..., indqn_D], 
+                        indims = [indims_d..., indims_D, indims_d..., indims_D],
+                        ifZ2=sitetype.ifZ2
+                        ) 
+    ΓA_new = ein"pludr,pdsy -> sluyr"(ΓAA_new, swap_dD) 
+
+ 
+    Γ[left] = ΓA_new
+    Γ[right] = ΓB_new
+ 
+    Snorm = norm(S)
+    S /= Snorm
+
+    λ[lm] = S
+      
+    return Snorm
+end
+
+function update_column!(ST, Γ, λ, Udτ, D_truc; whichbond)
+    sitetype, atype, dtype = ST.stype, ST.atype, ST.dtype
+    if whichbond =="down"
+        left = 1
+        right = 2
+        lm = 2
+    elseif whichbond =="up"
+        left = 2
+        right = 1
+        lm = 4
+    else
+        println("Wrong Position in update column!")
+    end 
+
+    ΓA = Γ[left]
+    ΓB = Γ[right]
+
+    D = ΓA.size[2]
+    d = Udτ.size[1]
+    
+    # d, D, d, D
+    indqn_D, indims_D = qndims(ΓB, 2)  
+    indqn_d, indims_d = getqrange(sitetype, d), getblockdims(sitetype, d)
+
+    swap_dD = U1swapgate(atype, dtype, d, D; 
+                        indqn = [indqn_d..., indqn_D, indqn_d..., indqn_D], 
+                        indims = [indims_d..., indims_D, indims_d..., indims_D],
+                        ifZ2=sitetype.ifZ2
+                        )   	 
+    # step 1: Fig.29(a)
+    ΓBB = ein"pludr,pxsl -> sxudr"(ΓB, swap_dD) 
+
+    # step 2, Fig.34(a): obtain Θ up  clockwise  [lurds]
+    Γ_0 = ein"((sludr,xl),yu),rz -> xyzds"(ΓA, λ[order(lm+1)], λ[order(lm+2)], λ[order(lm+3)])
+  
+    # step 2': Fig.29(c) SVD/QR with no truncation 
+    Γ_0 = reshape(Γ_0, D^3, D*d) # 
+    Q, R = qrpos(Γ_0)
+    
+    WA = reshape(Q, D, D, D, D*d) 
+    MA = reshape(R, D*d, D, d)
+
+    # step 3, Fig.34(a): clock-wise  down: [surdl] 
+    Γ_1 = ein"((pludr,rx),dy),zl -> puxyz"(ΓBB, λ[order(lm+1)], λ[order(lm+2)], λ[order(lm+3)])
+
+    # step 3': Fig.29(d) SVD/RQ with no truncation      
+    Q, R = qrpos(reshape(Γ_1, d*D, D^3)')
+    UB = reshape(Q', D*d, D, D, D)
+    NB = reshape(R', d, D, D*d)
+
+    # step 4, Fig.34(a) or Towards Fig B.1(II)
+    Θ0 = ein"(udp,dy),syx -> upsx"(MA, λ[lm], NB) # anticlock-wise, left 
+    Θ = ein"upsd,psxy -> uxyd"(Θ0, Udτ) # anticlock-wise 
+ 
+    shape = Θ.size
+    temp = reshape(Θ, shape[2]*shape[1], shape[3]*shape[4]) #  
+    U, S, V = svd!(temp; trunc=D_truc, middledir=1)  #  
+    MA_new = reshape(U, shape[1], shape[2], D_truc)
+    NB_new = reshape(V', D_truc, shape[3], shape[4])    
+    S = Diagonal(S)
+    
+
+    # step 5, Fig.(35)
+    inv_λ2 = invDiagU1Matrix(λ[order(lm+1)])
+    inv_λ3 = invDiagU1Matrix(λ[order(lm+2)])
+    inv_λ4 = invDiagU1Matrix(λ[order(lm+3)])
+
+    #  WA:(l,u,r,d  ),  l3, l4, l1
+    ΓA_new = ein"(((lurd,xl),yu),rz),dpw -> pxywz"(WA, inv_λ2, inv_λ3, inv_λ4, MA_new)  
+    # UB:(u,r,d,l)
+    ΓBB_new = ein"(((mpu,urdl),rx),dy),zl -> pzmyx"(NB_new, UB, inv_λ2, inv_λ3, inv_λ4)
+
+    # step 6, Fig.(32b)
+    indqn_D, indims_D = qndims(ΓBB_new, 2)  
+    swap_dD = U1swapgate(atype, dtype, d, D; 
+                        indqn = [indqn_d..., indqn_D, indqn_d..., indqn_D], 
+                        indims = [indims_d..., indims_D, indims_d..., indims_D],
+                        ifZ2=sitetype.ifZ2
+                        )   
+    ΓB_new = ein"pludr,pxsl -> sxudr"(ΓBB_new, swap_dD) 
+
+    Γ[left] = ΓA_new
+    Γ[right] = ΓB_new
+
+    Snorm = norm(S)
+    S /= Snorm
+ 
+    λ[lm] = S
+      
+    return  Snorm 
+end 
--- test/SU.jl
+++ test/SU.jl
@@ -1,13 +1,13 @@
 using ADFPEPS
-using ADFPEPS: initλΓ, evoGate
+using ADFPEPS: initλΓ, evoGate, update_row!, update_column!
 using TeneT
 using Test
 
 @testset "SU init" for symmetry in [:none, :U1], stype in [tJZ2()], atype in [Array], dtype in [ComplexF64]
     D = 3
     d = 2
-    SD = SymmetricType(Val(symmetry), stype, atype, dtype)
-    λ, Γ = initλΓ(SD, D, d)
+    ST = SymmetricType(Val(symmetry), stype, atype, dtype)
+    λ, Γ = initλΓ(ST, D, d)
     @test size(λ) == (4,)
     @test size(Γ) == (2,)
     @test size(λ[1]) == (D,D)
@@ -17,11 +17,93 @@ end
 @testset "SU evoGate" for symmetry in [:U1], stype in [tJZ2()], atype in [Array], dtype in [ComplexF64]
     model = tJ(1.0,1.0,1.0)
     d = 3
-    
+
     dτ = 0.1
-    SD = SymmetricType(Val(symmetry), stype, atype, dtype)
-    U_local, U_2sites_1 = evoGate(SD, model, dτ)
+    ST = SymmetricType(Val(symmetry), stype, atype, dtype)
+    U_2sites, U_local = evoGate(ST, model, dτ)
     
-    @test size(U_local) == (d,d)
-    @test size(U_2sites_1) == (d,d,d,d)
+    @test size(U_2sites) == (d,d,d,d)
+    @test size(U_local) == (d,d) 
+end
+
+@testset "SU update_row!" for symmetry in [:U1], stype in [tJZ2()], atype in [Array], dtype in [ComplexF64]
+    model = tJ(1.0,1.0,1.0)
+    d = 3
+    D = 3
+    dτ = 0.1
+    ST = SymmetricType(Val(symmetry), stype, atype, dtype)
+    U_2sites, U_local = evoGate(ST, model, dτ)
+
+    # update right bond, or 1
+    λ, Γ = initλΓ(ST, D, d)
+    λo, Γo = copy(λ), copy(Γ)
+    update_row!(ST, Γ, λ, U_2sites, D; whichbond = "right")
+    @test size(λ[1]) == (D,D)
+    @test size(Γ[1]) == (d,D,D,D,D)
+    @test size(Γ[2]) == (d,D,D,D,D)
+    
+    @test λ[1] != λo[1]
+    @test λ[2] == λo[2]
+    @test λ[3] == λo[3]
+    @test λ[4] == λo[4]
+
+    @test Γ[1] != Γo[1]
+    @test Γ[2] != Γo[2]
+
+    # update left bond, or 3
+    λ, Γ = initλΓ(ST, D, d)
+    λo, Γo = copy(λ), copy(Γ)
+    update_row!(ST, Γ, λ, U_2sites, D; whichbond = "left")
+    @test size(λ[3]) == (D,D)
+    @test size(Γ[1]) == (d,D,D,D,D)
+    @test size(Γ[2]) == (d,D,D,D,D)
+    
+    @test λ[1] == λo[1]
+    @test λ[2] == λo[2]
+    @test λ[3] != λo[3]
+    @test λ[4] == λo[4]
+
+    @test Γ[1] != Γo[1]
+    @test Γ[2] != Γo[2]
+end
+
+@testset "SU update_column!" for symmetry in [:U1], stype in [tJZ2()], atype in [Array], dtype in [ComplexF64]
+    model = tJ(1.0,1.0,1.0)
+    d = 3
+    D = 3
+    dτ = 0.1
+    ST = SymmetricType(Val(symmetry), stype, atype, dtype)
+    U_2sites, U_local = evoGate(ST, model, dτ)
+
+    # update up bond, or 4
+    λ, Γ = initλΓ(ST, D, d)
+    λo, Γo = copy(λ), copy(Γ)
+    update_column!(ST, Γ, λ, U_2sites, D; whichbond = "up")
+    @test size(λ[4]) == (D,D)
+    @test size(Γ[1]) == (d,D,D,D,D)
+    @test size(Γ[2]) == (d,D,D,D,D)
+    
+    @test λ[1] == λo[1]
+    @test λ[2] == λo[2]
+    @test λ[3] == λo[3]
+    @test λ[4] != λo[4]
+
+    @test Γ[1] != Γo[1]
+    @test Γ[2] != Γo[2]
+
+    # update down bond, or 2
+    λ, Γ = initλΓ(ST, D, d)
+    λo, Γo = copy(λ), copy(Γ)
+    update_column!(ST, Γ, λ, U_2sites, D; whichbond = "down")
+    @test size(λ[2]) == (D,D)
+    @test size(Γ[1]) == (d,D,D,D,D)
+    @test size(Γ[2]) == (d,D,D,D,D)
+    
+    @test λ[1] == λo[1]
+    @test λ[2] != λo[2]
+    @test λ[3] == λo[3]
+    @test λ[4] == λo[4]
+
+    @test Γ[1] != Γo[1]
+    @test Γ[2] != Γo[2]
 end
